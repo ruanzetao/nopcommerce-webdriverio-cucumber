ec93745bb73b3d8fd971121af96187493eef819a
NGUYEN XUAN SANG <ruanzetao@gmail.com>
2023-03-24 16:14:40 +0700

    fix bugs of Dashboard TCs

--- src/features/dashboard.feature
+++ src/features/dashboard.feature
@@ -4,4 +4,4 @@ Feature: The Dashboad of NopCommerce website
     @dashboard_TC1
   Scenario Outline: As a user, I can go to the Dashboad of NopCommerce
 
-    When I go to the dashboard page of NopCommerce
+    When I go to the dashboard page
--- src/step-definitions/dashboard.ts
+++ src/step-definitions/dashboard.ts
@@ -1,11 +1,12 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 
 import Page from './page.ts';
+import DashboardPage from '../../src/pageobjects/dashboard.page'
 
 const pages = {
-    dashboard: DashboardPage;
+    dashboard: DashboardPage
 }
 
-Given(/^I go to the (\w+) page of NopCommerce$/, async (page) => {
-    await pages[page].open();
+Given(/^I go to the dashboard page$/, async (page) => {
+    await pages['dashboard'].open();
 });
